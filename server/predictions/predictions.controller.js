6fdac465ecf27a16770960ed05db5e6a2140ae4b
nmelentjevs <nikita.melentjevs@n-fuse.co>
2019-09-07 21:32:43 +0300

    updated aws nodejs

--- server/predictions/predictions.controller.js
+++ server/predictions/predictions.controller.js
@@ -6,7 +6,7 @@ exports.addPrediction = (req, res) => {
   const { country, league, prediction, title } = req.body.prediction;
   const { user } = req.body;
   psql.query(
-    `INSERT INTO public.predictions(author, text, title, liked, disliked, country, league) VALUES ('${user}', '${prediction.replace(
+    `INSERT INTO predictions(author, text, title, liked, disliked, country, league) VALUES ('${user}', '${prediction.replace(
       "'",
       '*'
     )}', '${title}', '0', '0', '${country}', '${league}');`,
@@ -23,133 +23,133 @@ exports.getPredictions = async (req, res, next) => {
   const { action, filter } = req.params;
   console.log(req.params);
 
-  redisClient.get('predictions', async (err, values) => {
-    // if (values && action !== 'false' && filter !== 'all') {
-    //   console.log('Got from cache');
-    //   res.send(JSON.parse(values));
-    // } else {
-    console.log('Got from psql');
-    const leagues = [
-      'Premier League',
-      'Championship',
-      'EFL Cup',
-      'FA Cup',
-      'Ligue 1',
-      'Ligue 2',
-      'Coupe de la Ligue',
-      'La Liga',
-      'Segunda Division',
-      'Copa Del Rey',
-      'Bundesliga',
-      '2. Bundesliga',
-      'DFB Pokal',
-      'Seria A',
-      'Seria B',
-      'Coppa Italia',
-      'Champions League',
-      'Europa League',
-      'World Cup',
-      'Europe Cup',
-      'Copa America'
-    ];
+  // redisClient.get('predictions', async (err, values) => {
+  // if (values && action !== 'false' && filter !== 'all') {
+  //   console.log('Got from cache');
+  //   res.send(JSON.parse(values));
+  // } else {
+  console.log('Got from psql');
+  const leagues = [
+    'Premier League',
+    'Championship',
+    'EFL Cup',
+    'FA Cup',
+    'Ligue 1',
+    'Ligue 2',
+    'Coupe de la Ligue',
+    'La Liga',
+    'Segunda Division',
+    'Copa Del Rey',
+    'Bundesliga',
+    '2. Bundesliga',
+    'DFB Pokal',
+    'Seria A',
+    'Seria B',
+    'Coppa Italia',
+    'Champions League',
+    'Europa League',
+    'World Cup',
+    'Europe Cup',
+    'Copa America'
+  ];
 
-    const countries = [
-      'England',
-      'France',
-      'Spain',
-      'Germany',
-      'Italy',
-      'Europe',
-      'National'
-    ];
-    let filter;
+  const countries = [
+    'England',
+    'France',
+    'Spain',
+    'Germany',
+    'Italy',
+    'Europe',
+    'National'
+  ];
+  let filter;
 
-    isCountry = countries.includes(req.params.filter);
-    isLeague = leagues.includes(req.params.filter);
-    isUser = !isCountry && !isLeague;
+  isCountry = countries.includes(req.params.filter);
+  isLeague = leagues.includes(req.params.filter);
+  isUser = !isCountry && !isLeague;
 
-    (isCountry || isLeague || isUser) && !req.params.filter.match('^all*')
-      ? (filter = req.params.filter)
-      : (filter = false);
+  (isCountry || isLeague || isUser) && !req.params.filter.match('^all*')
+    ? (filter = req.params.filter)
+    : (filter = false);
 
-    // console.log(
-    //   'country:' + isCountry,
-    //   'league: ' + isLeague,
-    //   'user: ' + isUser,
-    //   'filter: ' + filter
-    // );
+  // console.log(
+  //   'country:' + isCountry,
+  //   'league: ' + isLeague,
+  //   'user: ' + isUser,
+  //   'filter: ' + filter
+  // );
 
-    await psql
-      .query(
-        `SELECT public.predictions.id, public.predictions.author, public.predictions.text, public.predictions.title, public.predictions.edited_on, public.predictions.league, public.predictions.country, public.predictions.wrote_on, users.username
-          FROM public.users
-          INNER JOIN public.predictions ON public.users.username = public.predictions.author  ${
+  await psql
+    .query(
+      `SELECT predictions.id, predictions.author, predictions.text, predictions.title, predictions.edited_on, predictions.league, predictions.country, predictions.wrote_on, users.username
+          FROM users
+          INNER JOIN predictions ON users.username = predictions.author  ${
             filter
               ? isCountry
-                ? `AND public.predictions.country = '${filter}'`
+                ? `AND predictions.country = '${filter}'`
                 : isLeague
-                ? `AND public.predictions.league = '${filter}'`
+                ? `AND predictions.league = '${filter}'`
                 : isUser
-                ? `AND public.predictions.author = '${filter}'`
+                ? `AND predictions.author = '${filter}'`
                 : ''
               : ''
           }
-          ORDER BY public.predictions.wrote_on;
+          ORDER BY predictions.wrote_on;
         `
-      )
-      .then(async predictions => {
-        await psql
-          .query(
-            `SELECT liker_username, post_id, emotion
+    )
+    .then(async predictions => {
+      await psql
+        .query(
+          `SELECT liker_username, post_id, emotion
               FROM (
                 Select liker_username, post_id, 'Like' as emotion
-                From public.likes
+                From likes
                 Union 
                 Select disliker_username, post_id, 'Dislike' as emotion
-                From public.dislikes
+                From dislikes
               ) As a
               Order by post_id
               `
-          )
-          .then(likes => {
-            predictions.rows.map(prediction => {
-              prediction.text = prediction.text.replace('*', "'");
-              prediction.liked = 0;
-              prediction.disliked = 0;
-              prediction.liked_by = [];
-              prediction.disliked_by = [];
-              likes.rows.filter(like => {
-                if (prediction.id === like.post_id) {
-                  if (like.emotion === 'Like') {
-                    prediction.liked += 1;
-                    prediction.liked_by.push(like.liker_username);
-                  } else {
-                    prediction.disliked += 1;
-                    prediction.disliked_by.push(like.liker_username);
-                  }
+        )
+        .then(likes => {
+          predictions.rows.map(prediction => {
+            prediction.text = prediction.text.replace('*', "'");
+            prediction.liked = 0;
+            prediction.disliked = 0;
+            prediction.liked_by = [];
+            prediction.disliked_by = [];
+            likes.rows.filter(like => {
+              if (prediction.id === like.post_id) {
+                if (like.emotion === 'Like') {
+                  prediction.liked += 1;
+                  prediction.liked_by.push(like.liker_username);
+                } else {
+                  prediction.disliked += 1;
+                  prediction.disliked_by.push(like.liker_username);
                 }
-              });
+              }
             });
-
-            // redisClient.setex(
-            //   'predictions',
-            //   60,
-            //   JSON.stringify(predictions.rows)
-            // );
-            console.log(predictions.rows);
-            res.json(predictions.rows);
           });
-      })
-      .catch(err => console.log(err));
-    // }
-  });
+
+          // redisClient.setex(
+          //   'predictions',
+          //   60,
+          //   JSON.stringify(predictions.rows)
+          // );
+          console.log(predictions.rows);
+          res.json(predictions.rows);
+        });
+    })
+    .catch(err => console.log(err));
+  // }
+  // });
 };
 
 exports.likePrediction = (req, res) => {
   const { post_id, like, username } = req.body;
   if (like == 1) {
     psql.query(
-      `INSERT INTO public.likes(post_id, liker_username) VALUES('${post_id}', '${username}');`,
+      `INSERT INTO likes(post_id, liker_username) VALUES('${post_id}', '${username}');`,
       (error, results) => {
         if (error) {
           res.status(500).json({ msg: error });
@@ -159,7 +159,7 @@ exports.likePrediction = (req, res) => {
     );
   } else {
     psql.query(
-      `INSERT INTO public.dislikes(post_id, disliker_username) VALUES('${post_id}', '${username}');`,
+      `INSERT INTO dislikes(post_id, disliker_username) VALUES('${post_id}', '${username}');`,
       (error, results) => {
         if (error) {
           res.status(500).json({ msg: error });
@@ -172,7 +172,7 @@ exports.likePrediction = (req, res) => {
 
 exports.editPrediction = (req, res) =>
   psql.query(
-    `UPDATE public.predictions text SET text='${
+    `UPDATE predictions text SET text='${
       req.body.text
     }', edited_on = '${new Date(
       Date('dd/mm/yyyy:HH:MM')
