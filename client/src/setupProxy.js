fd54e707a9d905ff9c5810c8690510de62dfcc06
nmelentjevs <nikita.melentjevs@n-fuse.co>
2019-09-07 12:30:25 +0300

    aws change

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,5 +1,5 @@
-const proxy = require('http-proxy-middleware');
+// const proxy = require('http-proxy-middleware');
 
-module.exports = function(app) {
-  app.use(proxy('/api/**', { target: 'http://127.0.0.1:3001' }));
-};
+// module.exports = function(app) {
+//   app.use(proxy('/api/**', { target: 'http://127.0.0.1:3001' }));
+// };
