6ab98b353af6d7e01cd7af97e2a23e4c45dcf60f
nmelentjevs <nikita.melentjevs@n-fuse.co>
2019-09-07 21:51:30 +0300

    redis configuration

--- server/predictions/predictions.controller.js
+++ server/predictions/predictions.controller.js
@@ -1,6 +1,6 @@
 const psql = require('../db/psqldb');
 
-// const { redisClient, redisPublisher } = require('../redis/redis');
+const { redisClient, redisPublisher } = require('../redis/redis');
 
 exports.addPrediction = (req, res) => {
   const { country, league, prediction, title } = req.body.prediction;
@@ -20,69 +20,68 @@ exports.addPrediction = (req, res) => {
 };
 
 exports.getPredictions = async (req, res, next) => {
-  // const { action, filter } = req.params;
-  const { actions } = req.params;
+  const { action, filter } = req.params;
   console.log(req.params);
 
-  // redisClient.get('predictions', async (err, values) => {
-  // if (values && action !== 'false' && filter !== 'all') {
-  //   console.log('Got from cache');
-  //   res.send(JSON.parse(values));
-  // } else {
-  console.log('Got from psql');
-  const leagues = [
-    'Premier League',
-    'Championship',
-    'EFL Cup',
-    'FA Cup',
-    'Ligue 1',
-    'Ligue 2',
-    'Coupe de la Ligue',
-    'La Liga',
-    'Segunda Division',
-    'Copa Del Rey',
-    'Bundesliga',
-    '2. Bundesliga',
-    'DFB Pokal',
-    'Seria A',
-    'Seria B',
-    'Coppa Italia',
-    'Champions League',
-    'Europa League',
-    'World Cup',
-    'Europe Cup',
-    'Copa America'
-  ];
+  redisClient.get('predictions', async (err, values) => {
+    if (values && action !== 'false' && filter !== 'all') {
+      console.log('Got from cache');
+      res.send(JSON.parse(values));
+    } else {
+      console.log('Got from psql');
+      const leagues = [
+        'Premier League',
+        'Championship',
+        'EFL Cup',
+        'FA Cup',
+        'Ligue 1',
+        'Ligue 2',
+        'Coupe de la Ligue',
+        'La Liga',
+        'Segunda Division',
+        'Copa Del Rey',
+        'Bundesliga',
+        '2. Bundesliga',
+        'DFB Pokal',
+        'Seria A',
+        'Seria B',
+        'Coppa Italia',
+        'Champions League',
+        'Europa League',
+        'World Cup',
+        'Europe Cup',
+        'Copa America'
+      ];
 
-  const countries = [
-    'England',
-    'France',
-    'Spain',
-    'Germany',
-    'Italy',
-    'Europe',
-    'National'
-  ];
-  let filter;
+      const countries = [
+        'England',
+        'France',
+        'Spain',
+        'Germany',
+        'Italy',
+        'Europe',
+        'National'
+      ];
+      let filter;
 
-  isCountry = countries.includes(req.params.filter);
-  isLeague = leagues.includes(req.params.filter);
-  isUser = !isCountry && !isLeague;
+      isCountry = countries.includes(req.params.filter);
+      isLeague = leagues.includes(req.params.filter);
+      isUser = !isCountry && !isLeague;
 
-  (isCountry || isLeague || isUser) && !req.params.filter.match('^all*')
-    ? (filter = req.params.filter)
-    : (filter = false);
+      (isCountry || isLeague || isUser) && !req.params.filter.match('^all*')
+        ? (filter = req.params.filter)
+        : (filter = false);
 
-  // console.log(
-  //   'country:' + isCountry,
-  //   'league: ' + isLeague,
-  //   'user: ' + isUser,
-  //   'filter: ' + filter
-  // );
+      // console.log(
+      //   'country:' + isCountry,
+      //   'league: ' + isLeague,
+      //   'user: ' + isUser,
+      //   'filter: ' + filter
+      // );
 
-  await psql
-    .query(
-      `SELECT predictions.id, predictions.author, predictions.text, predictions.title, predictions.edited_on, predictions.league, predictions.country, predictions.wrote_on, users.username
+      await psql
+        .query(
+          `SELECT predictions.id, predictions.author, predictions.text, predictions.title, predictions.edited_on, predictions.league, predictions.country, predictions.wrote_on, users.username
           FROM users
           INNER JOIN predictions ON users.username = predictions.author  ${
             filter
@@ -97,11 +96,11 @@ exports.getPredictions = async (req, res, next) => {
           }
           ORDER BY predictions.wrote_on;
         `
-    )
-    .then(async predictions => {
-      await psql
-        .query(
-          `SELECT liker_username, post_id, emotion
+        )
+        .then(async predictions => {
+          await psql
+            .query(
+              `SELECT liker_username, post_id, emotion
               FROM (
                 Select liker_username, post_id, 'Like' as emotion
                 From likes
@@ -111,39 +110,39 @@ exports.getPredictions = async (req, res, next) => {
               ) As a
               Order by post_id
               `
-        )
-        .then(likes => {
-          predictions.rows.map(prediction => {
-            prediction.text = prediction.text.replace('*', "'");
-            prediction.liked = 0;
-            prediction.disliked = 0;
-            prediction.liked_by = [];
-            prediction.disliked_by = [];
-            likes.rows.filter(like => {
-              if (prediction.id === like.post_id) {
-                if (like.emotion === 'Like') {
-                  prediction.liked += 1;
-                  prediction.liked_by.push(like.liker_username);
-                } else {
-                  prediction.disliked += 1;
-                  prediction.disliked_by.push(like.liker_username);
-                }
-              }
-            });
-          });
+            )
+            .then(likes => {
+              predictions.rows.map(prediction => {
+                prediction.text = prediction.text.replace('*', "'");
+                prediction.liked = 0;
+                prediction.disliked = 0;
+                prediction.liked_by = [];
+                prediction.disliked_by = [];
+                likes.rows.filter(like => {
+                  if (prediction.id === like.post_id) {
+                    if (like.emotion === 'Like') {
+                      prediction.liked += 1;
+                      prediction.liked_by.push(like.liker_username);
+                    } else {
+                      prediction.disliked += 1;
+                      prediction.disliked_by.push(like.liker_username);
+                    }
+                  }
+                });
+              });
 
-          // redisClient.setex(
-          //   'predictions',
-          //   60,
-          //   JSON.stringify(predictions.rows)
-          // );
-          console.log(predictions.rows);
-          res.json(predictions.rows);
-        });
-    })
-    .catch(err => console.log(err));
-  // }
-  // });
+              redisClient.setex(
+                'predictions',
+                60,
+                JSON.stringify(predictions.rows)
+              );
+              console.log(predictions.rows);
+              res.json(predictions.rows);
+            });
+        })
+        .catch(err => console.log(err));
+    }
+  });
 };
 
 exports.likePrediction = (req, res) => {
