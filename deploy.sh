3649e5e1afe29a681c99924e6bc0dd6cfb982b97
nmelentjevs <nikita.melentjevs@n-fuse.co>
2019-09-10 00:25:43 +0300

    gcloud travis config

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,13 @@ docker build -t docker/betmelon-client:latest -t kingusha/betmelon-client:$SHA -
 docker build -t docker/betmelon-server:latest -t kingusha/betmelon-server:$SHA -f ./server/Dockerfile ./server
 docker build -t docker/betmelon-postgres:latest -t kingusha/betmelon-postgres:$SHA -f ./psqldb/Dockerfile ./psqldb
 
-docker push kingusha/multi-client:latest
-docker push kingusha/multi-server:latest
-docker push kingusha/multi-postgres:latest
+docker push kingusha/betmelon-client:latest
+docker push kingusha/betmelon-server:latest
+docker push kingusha/betmelon-postgres:latest
 
-docker push kingusha/multi-client:$SHA
-docker push kingusha/multi-server:$SHA
-docker push kingusha/multi-postgres:$SHA
+docker push kingusha/betmelon-client:$SHA
+docker push kingusha/betmelon-server:$SHA
+docker push kingusha/betmelon-postgres:$SHA
 
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=kingusha/betmelon-server:$SHA
